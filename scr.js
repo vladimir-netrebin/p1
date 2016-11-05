d55aa322fd97994971a48f55951eaae099f130f2
vladimir-netrebin <vovan-n210@yandex.ru>
2016-11-06 01:05:20 +0300

    ili tak, esli udobnei

---
+++ scr.js
@@ -0,0 +1,33 @@
+﻿function show_pic() {
+    var c1_elem = document.getElementById("c1");
+    var c2_elem = document.getElementById("c2");
+    var c3_elem = document.getElementById("c3");
+    c1_elem.style.display = 'none';
+    c2_elem.style.display = 'block';
+    c3_elem.style.display = 'none';
+}
+function show_text() {
+    var c1_elem = document.getElementById("c1");
+    var c2_elem = document.getElementById("c2");
+    var c3_elem = document.getElementById("c3");
+    c1_elem.style.display = 'block';
+    c2_elem.style.display = 'none';
+    c3_elem.style.display = 'none';
+}
+function show_quiz() {
+    var c1_elem = document.getElementById("c1");
+    var c2_elem = document.getElementById("c2");
+    var c3_elem = document.getElementById("c3");
+    c1_elem.style.display = 'none';
+    c2_elem.style.display = 'none';
+    c3_elem.style.display = 'block';
+}
+function check() {
+    q1elem = document.getElementById('q1');
+    if (q1elem.checked) { alert("Yes") }
+    else { alert("No") }
+    reselem = document.getElementById('q3')
+    answer = reselem.value;
+    res.innerHTML = "Вы уверены, что " + answer + "?"
+}
+
